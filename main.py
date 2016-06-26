146ef5d728edd8c3ed739f133d6e062744284119
sairao210 <sairao210@gmail.com>
2016-06-26 16:48:07 +0530

    sign in feature added

--- main.py
+++ main.py
@@ -9,10 +9,27 @@ from kivy.uix.scatter import Scatter
 from kivy.uix.boxlayout import BoxLayout
 from pygments.lexers import CythonLexer
 from kivy.uix.codeinput import CodeInput
-
+import os
+import json
 class Myscreen(BoxLayout):
-    def log_prof(self,name,password):
-        pass
+    def log_prof(self,name,password,wel,sign_in):
+        f = open('./log_files/login_details.json',"r")
+        x = f.read()
+        result = json.loads(x)
+        flag = 0
+
+        for item in result['profs']:
+            if(name == item['name'] and flag==0):
+                flag = 1
+                if(password == item['pass']):
+                    wel.text = "Logged in as " + item['name']
+                else:
+                    wel.text =  "Password doesnt match"
+
+        if(flag == 0):
+            wel.text =  "Username doesnt match"
+
+        f.close()
     def register_prof(self):
         pass
 
