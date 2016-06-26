cc7edf63435d60507f3e9e81ec965f63a4dfca8e
sairao210 <sairao210@gmail.com>
2016-06-26 12:43:51 +0530

    this is my main gui file

---
+++ main.py
@@ -0,0 +1,21 @@
+import kivy
+kivy.require('1.9.1') # replace with your current kivy version !
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.boxlayout import BoxLayout
+from pygments.lexers import CythonLexer
+from kivy.uix.codeinput import CodeInput
+
+class Myscreen(BoxLayout):
+    pass
+
+class SasApp(App):
+    def build(self):
+        return Myscreen()
+
+if __name__ == '__main__':
+    SasApp().run()
